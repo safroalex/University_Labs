9855da6fb7c4a7ed4699e62121b48b54433ae880
safroalex <alexsafr.a@yandex.ru>
2023-03-06 23:25:14 +0300

    adding lab2 - need to do description in code

---
+++ OOP_lab1/lab2/classForCalling.java
@@ -0,0 +1,14 @@
+package lab2;
+
+public class classForCalling {
+    @annotation(1)
+    private void firstCalling() {
+        System.out.println("we call first method");
+    }
+    @annotation(2)
+    private void secondCalling() {
+        System.out.println("we call second method");
+    }
+    private void thirdMethod() {
+    }
+}
