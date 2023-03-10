90b24abdca9604179d768531a34f22e3f30faa94
safroalex <alexsafr.a@yandex.ru>
2023-03-10 23:18:49 +0300

    mm

---
+++ OOP_lab1/lab2/Main.java
@@ -0,0 +1,28 @@
+package lab2;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) {
+        classForCalling classForCalling = new classForCalling();
+        try {
+            callOurMethods(classForCalling);
+        }
+        catch (InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void callOurMethods(Object object) throws InvocationTargetException, IllegalAccessException {
+        Class clazz = object.getClass(); // определяем класс любого объекта, переданного в наш метод
+        for (Method method : clazz.getDeclaredMethods()) {  //возвращает массив методов класса, как private, так и protected
+            if (method.isAnnotationPresent(annotation.class)) {
+                for (int i = 0; i < method.getAnnotation(annotation.class).value(); i++) {
+                    method.setAccessible(true);
+                    method.invoke(object);
+                }
+            }
+        }
+    }
+}
