49d9269d7423cdd5fcb092cac9a767fde09b97b5
safroalex <alexsafr.a@ya.ru>
2023-03-13 13:28:01 +0300

    its ok

--- math_2sm/materialsForMath/CPP/cprogs/c_source/random.h
+++
@@ -1,9 +0,0 @@
-/* random.h
- * Header file for random number routines.
- * ------------------------------------------------------------
- */
-
-double random01 (int *restart);
-
-int    flip     (double p);
-
--- math_2sm/materialsForMath/CPP/cprogs/fortran/ga_build.bat
+++
@@ -1,3 +0,0 @@
-Rem build.bat
-fl rand01d.for random01.for
-fl ga_optd.for ga_opt.for random01.for
